709a03d112c65b9a3618c1fc74447d8540834b47
taldoflemis <coderflemis@gmail.com>
2023-03-20 18:36:31 -0300

    add users shit

---
+++ src/users/controller.rs
@@ -0,0 +1,33 @@
+use super::model::User;
+use super::service;
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Extension, Json, Router,
+};
+use sqlx::PgPool;
+
+async fn get_all(state: Extension<PgPool>) -> Result<Json<Vec<User>>, StatusCode> {
+    let users = service::get_all_users(&state.0).await.unwrap();
+    Ok(Json(users))
+}
+
+async fn create_user(state: Extension<PgPool>) -> Result<Json<User>, StatusCode> {
+    let user = service::create_user(&state.0).await.unwrap();
+    Ok(Json(user))
+}
+
+// async fn delete_user(state: Extension<PgPool>) -> Result<StatusCode, StatusCode> {
+//     let user = service::delete_user(&state.0).await.unwrap();
+//     Ok(StatusCode::OK)
+// }
+
+fn real_route() -> Router {
+    Router::new()
+        .route("/", get(get_all))
+        .route("/create", post(create_user))
+}
+
+pub fn route() -> Router {
+    Router::new().nest("/users", real_route())
+}
---
+++ src/users/service.rs
@@ -0,0 +1,21 @@
+use crate::users::model::User;
+use anyhow::Result;
+use sqlx::PgPool;
+
+pub async fn get_all_users(state: &sqlx::Pool<sqlx::Postgres>) -> Result<Vec<User>> {
+    let users = sqlx::query_as!(User, "SELECT * FROM users")
+        .fetch_all(state)
+        .await?;
+    Ok(users)
+}
+
+pub async fn create_user(state: &sqlx::Pool<sqlx::Postgres>) -> Result<User> {
+    let user = sqlx::query_as!(User, "INSERT INTO users (name) VALUES ('test') RETURNING *")
+        .fetch_one(state)
+        .await?;
+    Ok(user)
+}
+
+// pub async fn delete_user(state: &sqlx::Pool<sqlx::Postgres>) -> _ {
+//     let res = sqlx::query!("DELETE FROM users WHERE name = test").await;
+// }
