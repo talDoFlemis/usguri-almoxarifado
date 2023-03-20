5e6ae60ecd7265d9f5cd6bfcb726aee3684c2cdb
taldoflemis <coderflemis@gmail.com>
2023-03-20 18:36:36 -0300

    add lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,15 @@ use axum::{routing::get, Extension, Router};
 use sqlx::PgPool;
 use tower_http::trace::TraceLayer;
 
+mod users;
+
 pub async fn server(db: PgPool) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     let app = api_router()
         .layer(TraceLayer::new_for_http())
         .layer(Extension(db));
 
+    println!("Listening on http://{}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await?;
@@ -19,5 +22,5 @@ pub async fn server(db: PgPool) -> Result<()> {
 }
 
 fn api_router() -> Router {
-    Router::new().route("/", get(|| async { "hellow" }))
+    Router::new().merge(users::controller::route())
 }
