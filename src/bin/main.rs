35d11f047568f106601345677fa15ee4bf36e812
taldoflemis <coderflemis@gmail.com>
2023-03-21 07:46:11 -0300

    add better tracing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,19 @@
 use anyhow::{Context, Result};
 use sqlx::postgres::PgPoolOptions;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 use usguri_almoxarifado::server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenvy::dotenv().ok();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "usguri_almoxarifado=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let db_url = std::env::var("DATABASE_URL").context("DATABASE_URL not set")?;
     let db = PgPoolOptions::new()
         .max_connections(50)
