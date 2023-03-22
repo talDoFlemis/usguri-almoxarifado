ba43e470c6843a836a35338f8fb883e0583535c1
taldoflemis <coderflemis@gmail.com>
2023-03-22 15:00:23 -0300

    update user_model

--- src/models/user_model.rs
+++ src/models/user_model.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
@@ -5,16 +6,24 @@ use validator::Validate;
 pub struct User {
     pub id: i32,
     pub name: String,
+    pub email: String,
+    pub password: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Validate)]
 pub struct CreateUserDTO {
     #[validate(length(min = 1, message = "Can not be empty"))]
     pub name: String,
+    #[validate(email(message = "Invalid email"))]
+    pub email: String,
+    pub password: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Validate)]
 pub struct UpdateUserDTO {
     #[validate(length(min = 1, message = "Can not be empty"))]
     pub name: String,
+    pub password: String,
 }
